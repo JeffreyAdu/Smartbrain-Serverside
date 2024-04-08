0b58df5ec40b8076ef20945de1e0ba402f358209
Jeffrey <Jeffreyadu3@gmail.com>
2024-04-07 21:55:33 -0400

    checking register transaction error

--- controlers/register.js
+++ controlers/register.js
@@ -31,7 +31,7 @@ const handleRegister = (req, res, db, bcrypt) =>{
         })
         .then(trx.commit)
         .catch(trx.rollback)
-    }).catch(err => res.status(400).json('OOpsss Try again bro!'));
+    }).catch(err => res.status(400).json('OOpsss Try again bro!', err));
     
     
 }
