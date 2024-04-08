cfb595bfb816f1687645690223c123a93c45f6cc
Jeffrey <Jeffreyadu3@gmail.com>
2024-04-07 22:03:01 -0400

    still working on knex

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: true,
+      ssl: {rejectUnauthorized: false},
       host: process.env.DATABASE_HOST,
       port: process.env.DATABASE_PORT,
       user: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-    },
+    }
   });
 
 // const db = knex({
